3d09515b209805ccb755e4aa1fd8c09f9b7604c1
Superbrainbug <ivamajnar@foi.hr>
2016-11-26 19:58:27 +0100

    CollectionView for events created and designed

---
+++ VzLife/PhotosCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosCollectionViewCell.swift
+//  VzLife
+//
+//  Created by FOI on 26/11/16.
+//  Copyright © 2016 varazdinevents. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ VzLife/VierController.h
@@ -0,0 +1,13 @@
+//
+//  VierController.h
+//  VzLife
+//
+//  Created by FOI on 24/11/16.
+//  Copyright © 2016 varazdinevents. All rights reserved.
+//
+
+#ifndef VierController_h
+#define VierController_h
+
+
+#endif /* VierController_h */
