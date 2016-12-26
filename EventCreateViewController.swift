4a51a50d0a2399940fe36be557d7d8907059787a
Superbrainbug <ivamajnar@foi.hr>
2016-12-26 15:01:01 +0100

    Added Alert for uncompleted creation of event

--- EventCreateViewController.swift
+++ EventCreateViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class EventCreateViewController: UIViewController {
 
+    @IBOutlet weak var eventTitle: UITextField!
+    @IBOutlet weak var eventAbout: UITextField!
+    @IBOutlet weak var eventDate: UIDatePicker!
+    @IBOutlet weak var eventCategory: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +25,31 @@ class EventCreateViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func eventCreateButton(_ sender: Any) {
+        
+        let title = self.eventTitle.text
+        let about = self.eventAbout.text
+        let date  = self.eventDate.date
+        let category = self.eventCategory.text
+        
+        if((title?.isEmpty)! || (about?.isEmpty)! || (category?.isEmpty)!){
+            displayAlertMessage(userMessage: "Nije ispunjeno jedno od bitnih polja")
+            return
+        }
+        
+    }
+    
+    func displayAlertMessage(userMessage: String){
+        
+        var alert = UIAlertController(title: "Alert", message: userMessage, preferredStyle: UIAlertControllerStyle.alert)
+       
+        let action = UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil)
+        
+        alert.addAction(action)
+        self.present(alert, animated:true, completion: nil)
+    }
+    
+    
     /*
     // MARK: - Navigation
 
