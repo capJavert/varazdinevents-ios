3cc389495c6e033fd8e738bc81a7ed041364e0d7
capJavert <ante.baric3@gmail.com>
2017-01-27 19:02:57 +0100

    about view ui fixes

--- VzLife/AboutController.swift
+++ VzLife/AboutController.swift
@@ -11,11 +11,22 @@ import UIKit
 class AboutController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-          scrollView.contentSize = CGSize(width: self.view.frame.size.width, height: textView.frame.size.height + 150)
+        self.navigationItem.title = "O nama"
+        
+        //adjust text size
+        let fixedWidth = textView.frame.size.width
+        textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+        let newSize = textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+        var newFrame = textView.frame
+        newFrame.size = CGSize(width: max(newSize.width, fixedWidth), height: newSize.height)
+        textView.frame = newFrame;
+        
+        scrollView.contentSize = CGSize(width: self.view.frame.size.width, height: imageView.frame.size.height + textView.frame.size.height + 150)
     }
 
     override func didReceiveMemoryWarning() {
